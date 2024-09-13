019dadc7c04724abd3acd2a86bd74b2d1cae0dcd
sai <asaikrishnachary103@gmail.com>
2024-09-13 09:50:06 +0530

    sai

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,4 +1,3 @@
-// src/Header.js
 
 import React, { useState } from 'react';
 import logo from "../Images/clinic-logo.png"
@@ -22,12 +21,25 @@ function Navbar() {
 			<h3>MVM NATURE CURE HEALTH CENTRE</h3>
       </div>
 			<nav ref={navRef}>
-				<a style={{fontWeight:"700"}} href="/#">Home</a>
-				<a style={{fontWeight:"700"}} href="/#">About Us</a>
-				<a style={{fontWeight:"700"}} href="/#">Our Services</a>
-				<a style={{fontWeight:"700"}} href="/#">Our Doctors</a>
-        <a style={{fontWeight:"700"}} href="/#">Contact Us</a>
-        <a style={{fontWeight:"700"}} href="/#">Pricing</a>
+				<a style={{fontWeight:"700"}} href="/">Home</a>
+				<a style={{fontWeight:"700"}} href="/about">About Us</a>
+				 {/* Dropdown for Services */}
+				 <div className="dropdown">
+          <a style={{fontWeight:"700"}} href="/services" className="dropbtn">Our Services</a>
+          <div className="dropdown-content">
+            <a href="/services/hydrotherapy">Hydrotherapy</a>
+            <a href="/services/massage-therapy">Massage Therapy</a>
+            <a href="/services/yoga-therapy">Yoga Therapy</a>
+            <a href="/services/acupuncture">Acupuncture</a>
+            <a href="/services/dietrics">Dietrics</a>
+            <a href="/services/chiropractic">Chiropractic</a>
+            <a href="/services/physiotherapy">Physiotherapy</a>
+            <a href="/services/ayurvedic-consultations">Ayurvedic Consultations and Panchakarma</a>
+          </div>
+        </div>
+				<a style={{fontWeight:"700"}} href="/doctor">Our Doctors</a>
+        <a style={{fontWeight:"700"}} href="/contact">Contact Us</a>
+        <a style={{fontWeight:"700"}} href="/pricing">Pricing</a>
         {/* <button className='navbutton'>Book Appointment</button>
         <button className='navbutton'> +91 7259977110</button> */}
 				<button
